9de31b0cad949439fe83df38d5f957ace07a8813
Kawachi <kawachi.shusaku@opensesame-tech.com>
2017-05-08 13:31:03 +0900

    個人用Githubからまるっとソースコピー

---
+++ Parrot/Parrot/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+
+namespace Parrot {
+
+	/// <summary>
+	/// Web APIコンフィグ
+	/// </summary>
+	public static class WebApiConfig {
+
+		public static void Register( HttpConfiguration config ) {
+
+			// Web API の構成およびサービス
+
+			// Web API ルート
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name : "DefaultApi" ,
+				routeTemplate : "api/{controller}/{id}" ,
+				defaults : new { id = RouteParameter.Optional }
+			);
+
+		}
+
+	}
+
+}
